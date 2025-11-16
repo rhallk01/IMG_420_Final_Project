119c75f8ff90599ec41b1309c532102188a4251c
Haley Kloss <hrk85@nau.edu>
2025-11-16 14:06:40 -0700

    FIX: made it slightly less ugly

--- .godot/mono/temp/obj/Debug/Assignment_1.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/Assignment_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9641f7efeca1d3d9c856b2b4eb1117df6c885bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2575c2df405226002c704b0cb08955d204e07c19")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
